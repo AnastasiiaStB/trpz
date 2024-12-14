a642ec8d5663f9879a1c584163ecd2fc5de7eb2e
AnastasiiaStB <stepaniuk-borymska.anastasiia@lll.kpi.ua>
2024-12-14 07:40:55 +0200

    add lab7

--- src/main/java/org/example/DownloadManagerApp.java
+++ src/main/java/org/example/DownloadManagerApp.java
@@ -1,26 +1,24 @@
 package org.example;
 
-import org.example.command.*;
 import org.example.model.Download;
 import org.example.model.DownloadStatus;
-import org.example.observer.EventLogger;
-import org.example.observer.ProgressDisplay;
-
-import java.util.Arrays;
+import org.example.templatemethod.AbstractDownload;
+import org.example.templatemethod.FtpDownload;
+import org.example.templatemethod.HttpDownload;
+import org.example.templatemethod.HttpsDownload;
 
 public class DownloadManagerApp {
     public static void main(String[] args) {
-        Download download = new Download("file1.zip", "http://example.com/file1.zip", 1000, DownloadStatus.PENDING, 0);
+        Download httpFile = new Download("file1.txt", "http://example.com/file1", 1024, DownloadStatus.PENDING, 0.0);
+        Download httpsFile = new Download("file2.txt", "https://secure.com/file2", 2048, DownloadStatus.PENDING, 0.0);
+        Download ftpFile = new Download("file3.txt", "ftp://ftpserver.com/file3", 4096, DownloadStatus.PENDING, 0.0);
 
-        ProgressDisplay progressDisplay = new ProgressDisplay();
-        EventLogger eventLogger = new EventLogger();
+        AbstractDownload httpDownload = new HttpDownload(httpFile);
+        AbstractDownload httpsDownload = new HttpsDownload(httpsFile);
+        AbstractDownload ftpDownload = new FtpDownload(ftpFile);
 
-        download.addObserver(progressDisplay);
-        download.addObserver(eventLogger);
-
-        download.setStatus(DownloadStatus.IN_PROGRESS);
-        download.setProgress(25.0);
-        download.setProgress(50.0);
-        download.setStatus(DownloadStatus.COMPLETED);
+        httpDownload.executeDownload();
+        httpsDownload.executeDownload();
+        ftpDownload.executeDownload();
     }
 }
